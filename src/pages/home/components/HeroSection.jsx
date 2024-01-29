5d138d852c7edb52786e6447e9cb85c0dd640c5d
Daniel <danielkore1234@gmail.com>
2024-01-29 13:52:40 +0100

    update

---
+++ src/pages/home/components/HeroSection.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const HeroSection = () => {
+  return (
+    <div className="">
+      <div>
+        <h1 className="font-bold text-2xl">
+          Elevate Your Skills in an Exclusive Webinar Experience
+        </h1>
+        <p className="">
+          Join me as I share how I built a multi million Naira Academy in Lagos,
+          Nigeria. I am going to personally help one lucky participant earn up
+          to 1 Million Naira monthly in the first year.
+        </p>
+      </div>
+      <div>
+        <img src="https://res.cloudinary.com/doejcrfso/image/upload/v1706530316/webinar-project/Vector_1_zc61tv.png" alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default HeroSection;
