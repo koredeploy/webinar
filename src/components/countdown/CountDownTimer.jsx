a3499c532220eca878928020fd3731b3cb0b8318
Kingsley Solomon <kinxly@gmail.com>
2024-02-01 23:56:13 +0100

    fix: countdown timer mobile error

--- src/components/countdown/CountDownTimer.jsx
+++ src/components/countdown/CountDownTimer.jsx
@@ -6,20 +6,20 @@
 //     const [minutes, setMinutes] = useState(0);
 //     const [hours, setHours] = useState(0);
 //     const [days, setDays] = useState(0);
-  
+
 //     const getCurrentATime = () => {
 //       const deadline = "10:52 PM,February, 21, 2024";
-  
+
 //       const date = Date.parse(deadline) - Date.now();
-  
+
 //       setSeconds(Math.floor((date / 1000) % 60));
-  
+
 //       setMinutes(Math.floor((date / (1000 * 60)) % 60));
-  
+
 //       setHours(Math.floor((date / (1000 * 60 * 60)) % 12));
-  
+
 //       setDays(Math.floor(date / (1000 * 60 * 60 * 24)));
-  
+
 //       if (date <= 0) {
 //         // console.log("Countdown completed");
 //         setSeconds(0)
@@ -27,89 +27,92 @@
 //         setHours(0)
 //         setDays(0)
 //       }
-  
-  
+
 //     }
 //     useEffect(() => {
 //       setInterval(getCurrentATime, 1000);
-      
+
 //     }, []);
 
-    
-  
 //   return (
-    
+
 //   )
 // }
 
 // export default CountDownTimer
 
-// new timer 
+// new timer
 
-import  { useEffect, useState } from "react";
+import { useCallback } from "react";
+import { useEffect, useState } from "react";
 
 // eslint-disable-next-line react/prop-types
 const CountDownTimer = ({ timerStyle, heading }) => {
-  const [seconds, setSeconds] = useState(0);
-  const [minutes, setMinutes] = useState(0);
-  const [hours, setHours] = useState(0);
-  const [days, setDays] = useState(0);
+    const calculateTimeRemaining = useCallback(() => {
+        const deadline = "10:52 PM,February, 3, 2024";
+        const date = Date.parse(deadline) - Date.now();
 
-  const getCurrentATime = () => {
-    const deadline = "10:52 PM,February, 3, 2024";
-    const date = Date.parse(deadline) - Date.now();
+        const days = Math.floor(date / (1000 * 60 * 60 * 24));
+        const hours = Math.floor((date / (1000 * 60 * 60)) % 12);
+        const minutes = Math.floor((date / (1000 * 60)) % 60);
+        const seconds = Math.floor((date / 1000) % 60);
 
-    setSeconds(Math.floor((date / 1000) % 60));
-    setMinutes(Math.floor((date / (1000 * 60)) % 60));
-    setHours(Math.floor((date / (1000 * 60 * 60)) % 12));
-    setDays(Math.floor(date / (1000 * 60 * 60 * 24)));
+        return { days, hours, minutes, seconds };
+    }, []);
 
-    if (date <= 0) {
-      setSeconds(0);
-      setMinutes(0);
-      setHours(0);
-      setDays(0);
-    }
-  };
+    const [timeRemaining, setTimeRemaining] = useState(calculateTimeRemaining());
 
-  useEffect(() => {
-    const intervalId = setInterval(getCurrentATime, 1000);
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setTimeRemaining(calculateTimeRemaining());
+        }, 1000);
 
-    // Clean up the interval when the component is unmounted
-    return () => clearInterval(intervalId);
-  }, []);
+        return () => {
+            clearInterval(interval);
+        };
+    }, [calculateTimeRemaining]);
 
-  return (
-    <div className="py-3  ">
-    <h2 className={` ${heading} font-bold text-lg py-1 text-center lg:text-start`} >Webinar starts in:</h2>
-   <div className="flex flex-wrap justify-center  xl:justify-start items-center  gap-4 lg:gap-5">
-     <div className="flex flex-col content-center items-center">
-       <p className={` ${timerStyle} w-[60px] h-[60px] lg:w-[89px] lg:h-[81px] text-center border-2 rounded-md pt-2 lg:py-5 px-1 lg:px-6 text-[32px] font-semibold`}>
-         {days.toString().length == 2 ? `${days}` : `${days}`}
-       </p>{" "}
-       <span className="py-3 text-base md:text-xl">Days </span>
-     </div>
-     <div className="flex flex-col content-center items-center">
-       <p className={` ${timerStyle} w-[60px] h-[60px] lg:w-[89px] lg:h-[81px] text-center border-2 rounded-md pt-2 lg:py-5 px-1 lg:px-6 text-[32px] font-semibold`}>
-         {hours.toString().length == 2 ? `${hours} `: `${hours}`}
-       </p>
-       <span className="py-3 text-base md:text-xl">Hours </span>
-     </div>
-     <div className="flex flex-col content-center items-center">
-       <p className={` ${timerStyle} w-[60px] h-[60px] lg:w-[89px] lg:h-[81px] text-center border-2 rounded-md pt-2 lg:py-5 px-1 lg:px-6 text-[32px] font-semibold`}>
-         {minutes.toString().length == 2 ? `${minutes}` : `${minutes}`}
-       </p>
-       <span className="py-3 text-base md:text-xl ">Minutes </span>
-     </div>
-     <div className="flex flex-col content-center items-center">
-       <p  className={` ${timerStyle}  w-[60px] h-[60px] lg:w-[89px] lg:h-[81px] text-center border-2 rounded-md pt-2 lg:py-5 px-1 lg:px-6 text-[32px] font-semibold`}>
-         {seconds.toString().length == 2 ? `${seconds}` : `${seconds}`}
-       </p>
-       <span className="py-3 text-base md:text-xl">Seconds </span>
-     </div>
-   </div>
- </div>
-  );
+    // ==============================================
+
+    return (
+        <div className='py-3  '>
+            <h2 className={` ${heading} font-bold text-lg py-1 text-center lg:text-start`}>Webinar starts in:</h2>
+            <div className='flex flex-wrap justify-center  xl:justify-start items-center  gap-4 lg:gap-5'>
+                <div className='flex flex-col content-center items-center'>
+                    <p
+                        className={` ${timerStyle} w-[60px] h-[60px] lg:w-[89px] lg:h-[81px] text-center border-2 rounded-md pt-2 lg:py-5 px-1 lg:px-6 text-[32px] font-semibold`}
+                    >
+                        {timeRemaining.days}
+                    </p>{" "}
+                    <span className='py-3 text-base md:text-xl'>Days </span>
+                </div>
+                <div className='flex flex-col content-center items-center'>
+                    <p
+                        className={` ${timerStyle} w-[60px] h-[60px] lg:w-[89px] lg:h-[81px] text-center border-2 rounded-md pt-2 lg:py-5 px-1 lg:px-6 text-[32px] font-semibold`}
+                    >
+                        {timeRemaining.hours}
+                    </p>
+                    <span className='py-3 text-base md:text-xl'>Hours </span>
+                </div>
+                <div className='flex flex-col content-center items-center'>
+                    <p
+                        className={` ${timerStyle} w-[60px] h-[60px] lg:w-[89px] lg:h-[81px] text-center border-2 rounded-md pt-2 lg:py-5 px-1 lg:px-6 text-[32px] font-semibold`}
+                    >
+                        {timeRemaining.minutes}
+                    </p>
+                    <span className='py-3 text-base md:text-xl '>Minutes </span>
+                </div>
+                <div className='flex flex-col content-center items-center'>
+                    <p
+                        className={` ${timerStyle}  w-[60px] h-[60px] lg:w-[89px] lg:h-[81px] text-center border-2 rounded-md pt-2 lg:py-5 px-1 lg:px-6 text-[32px] font-semibold`}
+                    >
+                        {timeRemaining.seconds}
+                    </p>
+                    <span className='py-3 text-base md:text-xl'>Seconds </span>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default CountDownTimer;
