21c4d4434679a79d370c51a9d834b956ffc0e5d7
Daniel <danielkore1234@gmail.com>
2024-02-02 07:11:38 +0100

    fix: fixed loading state for btn

--- src/components/RegForm/RegistrationForm.jsx
+++ src/components/RegForm/RegistrationForm.jsx
@@ -151,7 +151,7 @@ const RegistrationForm = ({ isOpen, text, submitform, setInitalModalOpen }) => {
                                 leaveFrom='opacity-100 translate-y-0 sm:scale-100'
                                 leaveTo='opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95'
                             >
-                                <Dialog.Panel className='relative  top-1 md:top-10  transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg'>
+                                <Dialog.Panel className='relative  top-14 md:top-10  transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg'>
                                     <div className={`flex justify-end p-6`}>
                                         <MdCancel
                                             fontSize={`2rem`}
@@ -172,7 +172,7 @@ const RegistrationForm = ({ isOpen, text, submitform, setInitalModalOpen }) => {
                                         <button
                                             onClick={() => {
                                                 setInitalModalOpen(false);
-                                                setLoading(true)
+                                              
                                             }}
                                             style={{
                                                 background: "#2F40D5",
